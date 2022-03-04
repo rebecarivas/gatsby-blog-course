617a4d016a91d8144b7ffcc8c76d3b257ad64a25
rebecarivas <rebecarivas@gmail.com>
2022-03-04 18:11:01 -0300

    feat: adiciona o componente Profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Rebeca Rivas</h1>
+    <h2>Desenvolvedora Front End</h2>
+    <p>
+      Blog de tecnologia.
+    </p>
+  </div>
+)
+
+export default Profile
