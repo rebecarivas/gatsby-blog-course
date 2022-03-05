89ea6df12ced012f8aac2114c8854bfd7203f37f
rebecarivas <rebecarivas@gmail.com>
2022-03-05 13:52:12 -0300

    feat: adiciona estilo a pagina do post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,6 +2,13 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import {
+  PostHeader,
+  PostDate,
+  PostTitle,
+  PostDescription,
+  MainContent,
+} from "../components/Post/style"
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
@@ -9,8 +16,16 @@ const BlogPost = ({ data }) => {
   return (
     <Layout>
       <Seo title={post.frontmatter.title} />
-      <h1>Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      <PostHeader>
+        <PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </PostDate>
+        <PostTitle>{post.frontmatter.title}</PostTitle>
+        <PostDescription>{post.frontmatter.description}</PostDescription>
+      </PostHeader>
+      <MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </MainContent>
     </Layout>
   )
 }
@@ -20,8 +35,11 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
