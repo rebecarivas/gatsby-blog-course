71bea92e51e06716730c91a76563bcd4b7e3d964
rebecarivas <rebecarivas@gmail.com>
2022-03-05 09:06:22 -0300

    feat: adiciona component SocialLinks

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/rebecarivas`,
+  },
+  {
+    label: "Linkedin",
+    url: `https://www.linkedin.com/in/rebeca-rivas/`,
+  },
+  {
+    label: "Youtube",
+    url: `https://www.youtube.com/channel/UCyy-IxWl1xGeEp4FXGPx6BA`,
+  },
+]
+
+export default links
---
+++ src/components/SocialLinks/index.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+import Icons from "./Icons"
+import links from "./content"
+
+import {
+  SocialLinksWrapper,
+  SocialLinksList,
+  SocialLinksItem,
+  SocialLinksLink,
+  IconWrapper,
+} from "./style"
+
+const SocialLinks = () => (
+  <SocialLinksWrapper>
+    <SocialLinksList>
+      {links.map((link, i) => {
+        const Icon = Icons[link.label]
+
+        return (
+          <SocialLinksItem key={i}>
+            <SocialLinksLink
+              href={link.url}
+              title={link.label}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IconWrapper>
+                <Icon />
+              </IconWrapper>
+            </SocialLinksLink>
+          </SocialLinksItem>
+        )
+      })}
+    </SocialLinksList>
+  </SocialLinksWrapper>
+)
+
+export default SocialLinks
