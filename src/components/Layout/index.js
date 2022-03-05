a9f003ff7172dad29a304a09ff712debc78a8fcf
rebecarivas <rebecarivas@gmail.com>
2022-03-05 07:30:46 -0300

    feat: reftora syled components

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,19 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Profile from "../Profile"
-import styled from "styled-components"
 import GlobalStyles from "../../styles/global"
-
-const LayoutWrapper = styled.section`
-  display: flex;
-`
-
-const LayoutMain = styled.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`
+import { LayoutWrapper, LayoutMain } from "./style"
 
 const Layout = ({ children }) => {
   return (
