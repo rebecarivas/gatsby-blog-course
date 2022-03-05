e2305b702b4947cb22b4952bcd33e8ca174dabc4
rebecarivas <rebecarivas@gmail.com>
2022-03-05 13:46:24 -0300

    feat: integra o layout ao blog-post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,14 +1,17 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import Seo from "../components/seo"
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
-    <>
+    <Layout>
+      <Seo title={post.frontmatter.title} />
       <h1>Title: {post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
+    </Layout>
   )
 }
 
