3ff53a3ba906f27ef8155aefbd27a963618aee95
rebecarivas <rebecarivas@gmail.com>
2022-03-05 13:31:10 -0300

    feat: adiciona o componente Paginacao

--- gatsby-node.js
+++ gatsby-node.js
@@ -59,7 +59,7 @@ exports.createPages = ({ graphql, actions }) => {
         },
       })
     })
-    const postsPerPage = 6
+    const postsPerPage = 5
     const numPages = Math.ceil(posts.length / postsPerPage)
 
     Array.from({ length: numPages }).forEach((_, index) => {
