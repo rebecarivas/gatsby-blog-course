0faefff735ea39aa435ceea815bec56668ec2bd5
rebecarivas <rebecarivas@gmail.com>
2022-03-05 08:52:55 -0300

    feat: adiciona o componente Sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Profile from "../Profile"
+
+import { SidebarWrapper } from "./style"
+
+const Sidebar = () => (
+  <SidebarWrapper>
+    <Profile />
+  </SidebarWrapper>
+)
+
+export default Sidebar
