2a8d6dacfe10eeb9958f0cb837d331fb39ebc423
rebecarivas <rebecarivas@gmail.com>
2022-03-05 09:42:26 -0300

    feat: adiciona o componente PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import React from "react"
 
 import Layout from "../components/Layout"
+import PostItem from "../components/PostItem"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Home</h1>
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="30 de Julho de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
