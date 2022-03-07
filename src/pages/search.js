7416176667d72e16eadde9ecf25a5edd5866cd80
rebecarivas <rebecarivas@gmail.com>
2022-03-07 14:46:58 -0300

    feat: adiciona page e component Search

---
+++ src/pages/search.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import Seo from "../components/seo"
+import Search from "../components/Search"
+
+const SearchPage = () => (
+  <Layout>
+    <Seo title="search" />
+    <Search />
+  </Layout>
+)
+
+export default SearchPage
