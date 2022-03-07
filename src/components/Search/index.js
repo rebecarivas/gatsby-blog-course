30bcecf629a8519f770626e246633ef0d63fa910
rebecarivas <rebecarivas@gmail.com>
2022-03-07 15:30:02 -0300

    feat: adiciona estilo ao Search componente e configura o stats e SearchBox

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,7 +1,26 @@
 import React from "react"
+import algoliasearch from "algoliasearch/lite"
+import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
+import { SearchWrapper } from "./style"
 
-const Search = () => {
-  return <h1>Search Page</h1>
+const Search = ({ algolia }) => {
+  const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey)
+
+  return (
+    <SearchWrapper>
+      <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+        <SearchBox autoFocus translations={{ placeholder: "Pesquisar..." }} />
+        <Stats
+          translations={{
+            stats(nbHits, timeSpentMs) {
+              return `${nbHits} resultados encontrados em ${timeSpentMs}ms`
+            },
+          }}
+        />
+        <Hits />
+      </InstantSearch>
+    </SearchWrapper>
+  )
 }
 
 export default Search
