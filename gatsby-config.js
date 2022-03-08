8d9c302886d6309fb1f4a570ec0278e457569d56
rebecarivas <rebecarivas@gmail.com>
2022-03-08 17:29:56 -0300

    feat: configura o manisfest

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,13 +87,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Rebeca Rivas`,
+        short_name: `Rebeca Rivas`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#16202c`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        // theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
@@ -101,6 +101,6 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
