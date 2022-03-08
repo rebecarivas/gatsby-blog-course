ed444d0c8084b2f44f233b59e2826742b50f4aa3
rebecarivas <rebecarivas@gmail.com>
2022-03-08 19:03:29 -0300

    feat: adiciona conteudo ao about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,30 @@
 import React from "react"
+import Seo from "../components/seo"
+import Layout from "../components/Layout"
+import {
+  AboutContent,
+  AboutTitle,
+  AboutWrapper,
+} from "../components/About/style"
 
-const AboutPage = () => <h1>About Page</h1>
+const AboutPage = () => (
+  <Layout>
+    <Seo title="sobre mim" />
+    <AboutWrapper>
+      <AboutTitle>Sobre Mim</AboutTitle>
+
+      <AboutContent>
+        Sou apaixonada por desafios e também sou muito curiosa. Encontrei na
+        programação uma forma de manter meus olhos brilhando todo dia. Tenho
+        experiência em desenvolvimento mobile e front-end.
+      </AboutContent>
+
+      <AboutContent>
+        Sou Bióloga de formação com experiência acadêmica por 13 anos.
+        Atualmente curso Análise e Desenvolvimento de Sistemas.
+      </AboutContent>
+    </AboutWrapper>
+  </Layout>
+)
 
 export default AboutPage
